eceb55619b19f37b30b104e302d61cd15cc04251
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-12-29 13:28:35 -0500

    add expect function with .toBe

--- 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/index.js
+++ 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/index.js
@@ -5,14 +5,18 @@ let result, expected
 console.log("Adjust math.js to pass the test.")
 result = sum(3, 7)
 expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    }
+  }
 }
-
-
