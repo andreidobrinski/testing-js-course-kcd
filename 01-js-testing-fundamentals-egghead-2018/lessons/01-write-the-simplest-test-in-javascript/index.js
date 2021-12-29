f5be6c4ae84ffb42c08a72d6ef49351661427404
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-12-29 13:35:05 -0500

    add utils for sum and subtract tests

--- 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/index.js
+++ 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/index.js
@@ -3,13 +3,30 @@ const {sum, subtract} = require('./math')
 let result, expected
 
 console.log("Adjust math.js to pass the test.")
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function sumTest() {
+  result = sum(3, 7)
+  expected = 10
+  expect(result).toBe(expected)
+}
+test('sum adds numbers', sumTest)
+
+function subtractTest() {
+  result = subtract(7, 3)
+  expected = 4
+  expect(result).toBe(expected)
+}
+test('subtract subtracts numbers', subtractTest)
+
+function test(title, callback) {
+  try {
+    callback()
+    console.log(`PASS - ${title}`)
+  } catch (error) {
+    console.error(`FAIL - ${title}`)
+    console.error(error)
+  }
+}
 
 function expect(actual) {
   return {
