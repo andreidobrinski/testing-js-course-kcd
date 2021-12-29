88243b34718d8c5b2dfc61c6e9ed416d78741b46
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-12-29 13:25:48 -0500

    add tests for add and subtract

--- 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/math.js
+++ 01-js-testing-fundamentals-egghead-2018/lessons/01-write-the-simplest-test-in-javascript/math.js
@@ -1,6 +1,20 @@
 // sum is intentionally broken so you can see errors in the tests
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
+
+// test for add
+const result = sum(3, 7)
+const expected = 10
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
+
+// test for subtract
+const result = subtract(7, 3)
+const expected = 4
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
 
 // these are kinda pointless I know, but it's just to simulate an async function
 const sumAsync = (...args) => Promise.resolve(sum(...args))
