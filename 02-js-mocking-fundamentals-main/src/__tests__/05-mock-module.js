588129c2b62b3f66029b4eac07569417263c8a75
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-01-09 12:31:27 -0500

    mock modules

---
+++ 02-js-mocking-fundamentals-main/src/__tests__/05-mock-module.js
@@ -0,0 +1,26 @@
+const thumbWar = require('../thumb-war')
+const utilsMock = require('../utils')
+
+/*
+  jest.mock(
+    string: path to module (relative to where jest.mock is being called),
+    fn: mock implementation
+  )
+*/
+jest.mock('../utils', () => {
+  return {
+    getWinner: jest.fn((p1, p2) => p1)
+  }
+})
+
+test('returns winner', () => {
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
+  expect(winner).toBe('Kent C. Dodds')
+  expect(utilsMock.getWinner.mock.calls).toEqual([
+    ['Kent C. Dodds', 'Ken Wheeler'],
+    ['Kent C. Dodds', 'Ken Wheeler']
+  ])
+
+  // cleanup
+  utilsMock.getWinner.mockReset()
+})
