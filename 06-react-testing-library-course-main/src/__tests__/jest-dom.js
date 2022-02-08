9e1d8b8d20b0bfc1b717a7b7ea08d82a5bb72239
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-08 16:56:09 -0500

    extend expect

--- 06-react-testing-library-course-main/src/__tests__/jest-dom.js
+++ 06-react-testing-library-course-main/src/__tests__/jest-dom.js
@@ -2,6 +2,18 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import {FavoriteNumber} from '../favorite-number'
 
+// intead of this
+import { toHaveAttribute, toHaveTextContent } from '@testing-library/jest-dom'
+expect.extend({
+  toHaveAttribute,
+  toHaveTextContent
+})
+
+// we can do this
+import '@testing-library/jest-dom/extend-expect';
+
+// or add the above line to the jest config to automatically have the extends on every test
+
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
