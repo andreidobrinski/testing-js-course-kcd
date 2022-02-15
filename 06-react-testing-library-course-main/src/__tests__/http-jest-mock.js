e4e9f5b46706674f51f8ecef178272fc922d1851
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-15 18:11:41 -0500

    mock API call

--- 06-react-testing-library-course-main/src/__tests__/http-jest-mock.js
+++ 06-react-testing-library-course-main/src/__tests__/http-jest-mock.js
@@ -1,19 +1,25 @@
 import * as React from 'react'
 import {render, screen, waitFor} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+// use alias to make it more clear that it's a mock
 import {loadGreeting as mockLoadGreeting} from '../api'
 import {GreetingLoader} from '../greeting-loader-01-mocking'
 
+// replaces all exported functions with jest mock functions
 jest.mock('../api')
 
 test('loads greetings on click', async () => {
   const testGreeting = 'TEST_GREETING'
+  // mocks API return
   mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
   render(<GreetingLoader />)
   const nameInput = screen.getByLabelText(/name/i)
   const loadButton = screen.getByText(/load/i)
   userEvent.type(nameInput, 'Mary')
   userEvent.click(loadButton)
+  // if a function is being mocked, it's good practice to:
+  // test that it's been called with the correct arg and
+  // test that it's been called (only) once
   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
   await waitFor(() =>
