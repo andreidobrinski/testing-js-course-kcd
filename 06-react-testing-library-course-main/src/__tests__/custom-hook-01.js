5bdf1fa426b26f287890bdac1b2c2e3dd3e52ec3
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-16 22:01:46 -0500

    test a custom hook with a mock function component

--- 06-react-testing-library-course-main/src/__tests__/custom-hook-01.js
+++ 06-react-testing-library-course-main/src/__tests__/custom-hook-01.js
@@ -3,6 +3,7 @@ import {render, act} from '@testing-library/react'
 import {useCounter} from '../use-counter'
 
 test('exposes the count and increment/decrement functions', () => {
+  // declare result here so that it can be accesible outside of the TestComponent
   let result
   function TestComponent() {
     result = useCounter()
@@ -10,6 +11,8 @@ test('exposes the count and increment/decrement functions', () => {
   }
   render(<TestComponent />)
   expect(result.count).toBe(0)
+  // normally you won't need to use act manually since everything is wrapped in act already
+  // this is necessary since we're reaching in to call increment directly
   act(() => result.increment())
   expect(result.count).toBe(1)
   act(() => result.decrement())
