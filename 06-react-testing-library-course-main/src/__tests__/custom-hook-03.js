26328342c40b84e4aad3bc9c3b07f7aa67c491e5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-02-16 22:08:38 -0500

    test hook with renderHook fn

--- 06-react-testing-library-course-main/src/__tests__/custom-hook-03.js
+++ 06-react-testing-library-course-main/src/__tests__/custom-hook-03.js
@@ -2,6 +2,7 @@ import {renderHook, act} from '@testing-library/react-hooks'
 import {useCounter} from '../use-counter'
 
 test('exposes the count and increment/decrement functions', () => {
+  // result needs to be destructured
   const {result} = renderHook(useCounter)
   expect(result.current.count).toBe(0)
   act(() => result.current.increment())
@@ -11,6 +12,7 @@ test('exposes the count and increment/decrement functions', () => {
 })
 
 test('allows customization of the initial count', () => {
+  // passing options works the same way that it did with the custom hook implementation
   const {result} = renderHook(useCounter, {initialProps: {initialCount: 3}})
   expect(result.current.count).toBe(3)
 })
