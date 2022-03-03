e39349ee8cfd2634eff3d89d6f0b793a2b093d50
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-03-02 20:41:36 -0500

    refactor jest-in-case with casify function

--- 08-testing-node-apps-main/src/utils/__tests__/auth.exercise.js
+++ 08-testing-node-apps-main/src/utils/__tests__/auth.exercise.js
@@ -99,3 +99,39 @@ cases(
     },
   }
 )
+
+// Solution 4: refactor jest-in-case for better error messages
+
+function casify(obj) {
+  return Object.entries(obj).map(([name, password]) => {
+    return {
+      name: `${password} - ${name}`,
+      password,
+    }
+  })
+}
+
+cases(
+  'isPasswordAllowed: valid passwords',
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(true)
+  },
+  casify({
+    'validPassword':'!aBc123',
+  })
+)
+
+cases(
+  'isPasswordAllowed: invalid passwords',
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(false)
+  },
+  casify({
+    'too short':'a2c!',
+    'no letters':'123456!',
+    'no numbers':'ABCdef!',
+    'no uppercase letters':'abc123!',
+    'no lowercase letters':'ABC123!',
+    'no non-alphanumeric characters':'ABCdef123',
+  })
+)
