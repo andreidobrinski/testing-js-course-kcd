6633bbc8ee19f53db90d275ab8fa675afdef781d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-03-18 16:45:19 -0400

    setup tests for node server

--- 08-testing-node-apps-main/src/__tests__/auth.exercise.js
+++ 08-testing-node-apps-main/src/__tests__/auth.exercise.js
@@ -1,24 +1,31 @@
 // Testing Authentication API Routes
 
-// 🐨 import the things you'll need
-// 💰 here, I'll just give them to you. You're welcome
-// import axios from 'axios'
-// import {resetDb} from 'utils/db-utils'
-// import * as generate from 'utils/generate'
-// import startServer from '../start'
+import axios from 'axios'
+import {resetDb} from 'utils/db-utils'
+import * as generate from 'utils/generate'
+import startServer from '../start'
 
-// 🐨 you'll need to start/stop the server using beforeAll and afterAll
-// 💰 This might be helpful: server = await startServer({port: 8000})
+let server
 
-// 🐨 beforeEach test in this file we want to reset the database
+beforeAll(async () => {
+  server = await startServer({port: 8000})
+})
+
+afterAll(async () => {
+  await server.close()
+})
+
+beforeEach(() => {
+  resetDb()
+})
 
 test('auth flow', async () => {
-  // 🐨 get a username and password from generate.loginForm()
-  //
-  // register
-  // 🐨 use axios.post to post the username and password to the registration endpoint
-  // 💰 http://localhost:8000/api/auth/register
-  //
+  const { username, password } = generate.loginForm()
+  const response = await axios.post('http://localhost:8000/api/auth/register', {
+    username,
+    password
+  })
+  
   // 🐨 assert that the result you get back is correct
   // 💰 it'll have an id and a token that will be random every time.
   // You can either only check that `result.data.user.username` is correct, or
