e98a017116b155a0190148c49e3dd437128e3438
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-03-20 12:35:07 -0400

    test create of a new list item

--- 08-testing-node-apps-main/src/__tests__/list-items.exercise.js
+++ 08-testing-node-apps-main/src/__tests__/list-items.exercise.js
@@ -31,18 +31,25 @@ async function setup() {
 
 test('listItem CRUD', async () => {
   const {testUser, authAPI} = await setup()
+  const book = generate.buildBook()
 
   // 🐨 create a book object and insert it into the database
   // 💰 use generate.buildBook and await booksDB.insert
+  await booksDB.insert(book)
 
   // CREATE
   // 🐨 create a new list-item by posting to the list-items endpoint with a bookId
   // 💰 the data you send should be: {bookId: book.id}
+  const cData = await authAPI.post('list-items', { bookId: book.id })
 
   // 🐨 assert that the data you get back is correct
   // 💰 it should have an ownerId (testUser.id) and a bookId (book.id)
   // 💰 if you don't want to assert on all the other properties, you can use
   // toMatchObject: https://jestjs.io/docs/en/expect#tomatchobjectobject
+  expect(cData.listItem).toMatchObject({
+    ownerId: testUser.id,
+    bookId: book.id
+  })
 
   // 💰 you might find this useful for the future requests:
   // const listItemId = cData.listItem.id
